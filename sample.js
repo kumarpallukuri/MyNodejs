730bafd60e1a568805126ebd29f9d131a759a71b
kumar <PALLUKKX@LNGDAYD-6002647.legal.regn.net>
2016-05-20 15:10:48 -0400

    changes

--- sample.js
+++ sample.js
@@ -2,3 +2,14 @@ console.log("hii")
 var a = 5;
 var b = 10;
 console.log(a + b);
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+})
