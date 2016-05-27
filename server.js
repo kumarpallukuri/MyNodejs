cbc879e19e106af7787b51e09c621f66f159cc13
kumarpallukuri <ksware3@gmail.com>
2016-05-27 15:37:47 -0400

    Add files via upload
    server file changes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var app = express()
  var path = require("path");
 app.use(express.static(__dirname + '/public'));
  app.use(bodyParser.json());
+ 
  app.get('/contactlist',function(req,res){
 	db.contactlist.find(function(err,docs){
 		console.log(docs);
@@ -14,11 +15,46 @@ app.use(express.static(__dirname + '/public'));
 	});
  });
  
+  app.get('/contactlist/email',function(req,res){
+	  console.log("method called -->")
+	db.contactlist.find(function(err,docs){
+		console.log(docs);
+		
+	});
+ });
  app.post('/contactlist',function(req,res){
 	console.log(req.body); 
 	db.contactlist.insert(req.body,function(err,doc){
 		res.json(doc);
 	});
+ });
+ 
+ app.delete('/contactlist/:id',function(req,res){
+	 var id = req.params.id;
+	 console.log(id);
+	 db.contactlist.remove({_id: mongojs.ObjectId(id)},function(err,doc){
+		res.json(doc);
+	});
+ });
+ 
+ app.get('/contactlist/:id',function(req,res){
+	 var id = req.params.id;
+	 console.log(id);
+	 db.contactlist.findOne({_id: mongojs.ObjectId(id)},function(err,doc){
+				res.json(doc);
+	});
+ });
+ 
+ app.put('/contactlist/:id',function(req,res){
+	 var id = req.params.id;
+	 console.log(req.body.name);
+	
+	
+	db.contactlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
+        update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+        new: true}, function(err, doc) {
+            res.json(doc);
+        });
  });
 app.listen(3000);
 
